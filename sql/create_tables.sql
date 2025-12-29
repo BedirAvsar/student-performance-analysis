576138b81725b0c622c0bfadabb85cd830996795
Bedir Avsar <113343991+BedirAvsar@users.noreply.github.com>
2025-12-29 03:20:58 +0300

    Add files via upload

---
+++ sql/create_tables.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS student_performance;
+
+CREATE TABLE student_performance (
+    student_id INTEGER,
+    age INTEGER,
+    gender INTEGER,
+    ethnicity INTEGER,
+    parental_education INTEGER,
+    study_time_weekly REAL,
+    absences INTEGER,
+    tutoring INTEGER,
+    parental_support INTEGER,
+    extracurricular INTEGER,
+    sports INTEGER,
+    music INTEGER,
+    volunteering INTEGER,
+    gpa REAL,
+    grade_class REAL
+);
